58d9051393d39f2d4a57ac0e2736aa64a1329c08
JirenXmoksh <mokshsingh4@gmail.com>
2024-10-04 13:03:30 +0530

    Strength section along with some minor UI improvements

--- scripting/aboutus.js
+++ scripting/aboutus.js
@@ -2,21 +2,22 @@
 let mybutton = document.getElementById("returnToTop");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+// window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "none";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     mybutton.style.display = "none";
+//   } else {
+//     mybutton.style.display = "none";
+//   }
+// }
 
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
 var tl2 = gsap.timeline();
 tl2.from(".navbar .container a", {
     y: 50,
